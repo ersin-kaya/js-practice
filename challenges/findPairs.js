b27669bfeda1f6d0254fd92f5efc6e2dc33ee336
ersin <ersin-kaya@outlook.com.tr>
2025-03-20 12:08:31 +0300

    Find pairs

---
+++ challenges/findPairs.js
@@ -0,0 +1,33 @@
+// Not optimized
+function findPairs(arr, target) {
+  let resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        let newArr = [arr[i], arr[j]];
+
+        let alreadyExists = false;
+        for (let k = 0; k < resultArr.length; k++) {
+          if (resultArr[k][0] === newArr[0] && resultArr[k][1] === newArr[1]) {
+            alreadyExists = true;
+            break;
+          }
+        }
+
+        if (!alreadyExists) {
+          resultArr[resultArr.length] = newArr;
+        }
+      }
+    }
+  }
+  return resultArr;
+}
+
+console.log(findPairs([1, 2, 3, 4, 5], 6)); // Output: [[1, 5], [2, 4]]
+
+console.log(findPairs([2, 4, 3, 7, 5, 8], 10)); // Output: [[2, 8], [3, 7]]
+
+console.log(findPairs([1, 1, 1, 2, 2, 3, 4], 3)); // Output: [[1, 2]]
+
+console.log(findPairs([0, -1, 2, -3, 1], -2)); // Output: [[-3, 1]]
